978108a49c2cb43fed43557cfad9fb00a051a919
Juan Felipe Alves Flores <vilajjlol@gmail.com>
2024-09-24 01:19:30 -0300

    :sparkles: feat: 6/13 Section 05: Kotlin básico - Conceitos iniciais.

---
+++ Section 05 - Kotlin básico - Conceitos iniciais/Projects/kotlin/src/Comentario.kt
@@ -0,0 +1,12 @@
+/**
+ * Documentação de classe, método ou propriedade.
+ */
+fun main() {
+    println("Hello, new world!")
+
+    // Comentário de linha única
+
+    /*
+       Comentário de linhas múltiplas
+    */
+}
---
+++ Section 05 - Kotlin básico - Conceitos iniciais/Projects/kotlin/src/DeclaracaoVariavel.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var nome = "Juan Felipe Alves Flores"
+    var profissao = "Desenvolvedor de software"
+
+    println("$nome - $profissao")
+}
